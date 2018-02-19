84b5d639f837a209701a630ef7b7410027e49f62
tabithak <tkarcher@expedia.com>
2018-02-19 11:23:07 -0800

    all the things
    creation

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+ // Your code goes here
+ ...
+ // Click handlers should look like this
+ $(‘body’).on(‘click’, ‘.some-css-selector’, function(){
+   // Do something in response to click
+   ...
+ });
+});
